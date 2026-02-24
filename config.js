041d588e80ae2806f8b6853e59325808595dcf66
ang-shiqi <angshiqi@gmail.com>
2026-02-24 17:15:40 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAYc0LgnDRRtBC-JFBc2XLU-yxFEaxaduM",
+  authDomain: "teo-calendar-4958b.firebaseapp.com",
+  projectId: "teo-calendar-4958b",
+  storageBucket: "teo-calendar-4958b.firebasestorage.app",
+  messagingSenderId: "468616135260",
+  appId: "1:468616135260:web:0d1c6bebcad79f1dd7dc99"
+};
